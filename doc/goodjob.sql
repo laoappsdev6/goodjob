c39e1344b3253f361e0ce154677c56b26a4df36e
laoappsdev6 <laoappsdev6@gmail.com>
2021-08-02 22:38:28 +0700

    udpate db

--- doc/goodjob.sql
+++ doc/goodjob.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 02, 2021 at 05:31 PM
+-- Generation Time: Aug 02, 2021 at 05:38 PM
 -- Server version: 10.4.17-MariaDB
 -- PHP Version: 8.0.2
 
