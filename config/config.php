6eda1e1d1df1f1c4bcbec4463b82e9bec95721b3
laoappsdev6 <laoappsdev6@gmail.com>
2021-08-07 11:40:26 +0700

    update

--- config/config.php
+++ config/config.php
@@ -2,7 +2,7 @@
 define("MY_PATH", "../image/");
 define("dbhost", "localhost");
 define("dbuser", "root");
-define("dbpass", "");
-// define("dbpass", "@Laoitstartup777");
+// define("dbpass", "");
+define("dbpass", "@Laoitstartup777");
 define("dbname", "goodjob");
 define("dbcharset", "utf8");
